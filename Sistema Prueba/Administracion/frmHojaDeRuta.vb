4fd210a2e494d3ff37040bdd567f0b2f4bcde968
Franco Molina Gonzalez <165958875+FrancoMG@users.noreply.github.com>
2025-06-12 19:22:31 -0300

    Conexión a base de datos

--- Sistema Prueba/Administracion/frmHojaDeRuta.vb
+++ Sistema Prueba/Administracion/frmHojaDeRuta.vb
@@ -7,4 +7,8 @@
     Private Sub lblApellidoCliente_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblApellidoCliente.Click, lblApellidoCliente2.Click
 
     End Sub
+
+    Private Sub frmHojaDeRuta_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
